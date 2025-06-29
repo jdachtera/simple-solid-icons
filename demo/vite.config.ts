eab997da19053f91239db05bb84f256ccb14ddfd
Jascha Dachtera <jascha.dachtera@gmail.com>
2025-06-29 11:01:36 +0200

    fix: allow Vite to resolve imports from parent src and preserve symlinks for Netlify build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { join } from 'path'
 
 export default defineConfig({
   plugins: [solid()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    fs: {
+      allow: [
+        '.',
+        '..',
+        join(__dirname, '../src'),
+        join(__dirname, '..')
+      ]
+    }
+  },
+  resolve: {
+    preserveSymlinks: true
+  },
   build: { target: 'esnext' },
 })
